1b21ba768a46c026e922e54d1fde2b97353dc51b
Rafael Garcia Sagastume <rafael.garcia@ciberc.com>
2023-07-25 13:56:09 -0600

    feat: Add function to render the template

--- ciberc_l3vpn_notify/core.py
+++ ciberc_l3vpn_notify/core.py
@@ -5,9 +5,25 @@ Module for global variables and common functions
 import pkg_resources
 from nornir import InitNornir
 from nornir.core.filter import F
+from nornir_jinja2.plugins.tasks import template_file
 
 template_path = pkg_resources.resource_filename(
     'ciberc_l3vpn_notify', 'templates')
+
+
+def render_template(task):
+    """
+    Render template file
+    """
+    result = task.run(
+        task=template_file,
+        name="Render template",
+        template=f"{template_path}/{task.host.platform}.j2",
+        path=f"{template_path}",
+        **task.host,
+    )
+
+    return result[0].result
 
 
 def automated_init(device):
