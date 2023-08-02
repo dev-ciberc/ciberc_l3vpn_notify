008a4b1f9e2c359708fb5c00ca58314dd61cf7f4
Rafael Garcia Sagastume <rafael.garcia@ciberc.com>
2023-08-02 11:07:21 -0600

    fix: install version

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 python3 setup.py bdist_wheel
 
 # Instalar el paquete
-python3 -m pip install dist/ciberc_l3vpn_notify-0.1-py3-none-any.whl --force-reinstall
+python3 -m pip install dist/ciberc_l3vpn_notify-0.1.1-py3-none-any.whl --force-reinstall
