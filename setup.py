cd716cf6150992b8f993aeacf7dec217f7b6d9ee
Rafael Garcia Sagastume <rafael.garcia@ciberc.com>
2023-08-02 10:43:28 -0600

    feat: Add project dependencies including nornir, napalm, and webexteamsbot

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf8") as fh:
 
 setup(
     name='ciberc_l3vpn_notify',
-    version='0.1.0',
+    version='0.1.1',
     description='CiberC Automated Cases',
     author='Rafael Fernando Garcia Sagastume',
     packages=find_packages(exclude=["tests"]),
@@ -16,7 +16,6 @@ setup(
         'ciberc_l3vpn_notify': [
             'templates/config-templates/*',
             'templates/rollback-templates/*',
-            'templates/napalm-process/*',
         ]
     },
     include_package_data=True,
@@ -26,5 +25,20 @@ setup(
         ]
     },
     long_description=long_description,
-    long_description_content_type="text/markdown"
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'nornir==3.3.0',
+        'nornir-jinja2==0.2.0',
+        'nornir-netmiko==1.0.0',
+        'nornir_napalm==0.4.0',
+        'nornir_utils==0.2.0',
+        'napalm==4.1.0',
+        'napalm-huawei-vrp==1.1.0',
+        'typer==0.9.0',
+        'setuptools==68.0.0',
+        'wheel==0.41.0',
+        'webexteamsbot==0.1.4.2',
+        'tqdm==4.65.0',
+        'pylint==2.17.5',
+    ],
 )
